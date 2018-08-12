37056b486050c7be50f1eca56b39cd9c1045d924
CongHuynh <40530561+CongHuynh@users.noreply.github.com>
2018-08-12 10:27:33 +0700

    jquery
    jquery

---
+++ jquery/js/common/common.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+$('a').click(function(){ //onclick	
+	$('#demo1').hide(); // document.getElementById('demo1)
+						// hide() = style.display = none;
+	$('.demo2').hide();
+	
+});
+$("#show").click(function(){
+    $("p").show();
+});
---
+++ jquery/js/common/commontext.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+$('#changeContent .change1').click(function(){
+	$('.demo1').text('Hello a');
+});
+$('change2').click(function(){
+	$('img').attr('src' , 'img/pic_bulbon.gif')
+});
+$('change3').click(function(){
+	$('img').attr('src' , 'img/pic_bulboff.gif')
+});
