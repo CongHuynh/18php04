270fda0dba931e4716b6b90d4934213d03f1f2f9
CongHuynh <40530561+CongHuynh@users.noreply.github.com>
2018-08-16 18:27:15 +0700

    bai tap
    bai tap

---
+++ jquery-ex/js/common/common.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+$('#yes , #no').click(function(){
+        
+	if($('#yes').text() == 'Có'){
+		$('.demo').text('Có');
+	}else{
+		$('.demo').text('Có');
+	}
+});
---
+++ jquery-ex/js/common/common1.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+$('#accost').click(function(){
+	  
+		$('.boy').animate({left:'450px'},2000);
+    	$('.girl').animate({right:'250px'},2000);
+	
+});
+$('#flower').click(function(){
+	  
+		$('.boy').attr('src','img/flower.jpg');
+	
+	
+});
